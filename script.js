9c0e6b39dbd987a72bc9c90fdd6fc899a81312c7
Ka-Soares <karolinasoares276@gmail.com>
2023-10-24 09:53:59 -0300

    [Front] Incluindo CSS, menu hamburger e icones de modo dark

---
+++ script.js
@@ -0,0 +1,14 @@
+:root{
+    --back-color:  rgba(245,136,35,255);
+    --menu-color: rgb(248, 189, 62);
+    --font-color:   rgba(245,136,35,255);;
+    --font-color1: rgba(87,53,18,255);
+    --sun-moon: rgb(255, 255, 255);
+}
+
+.light-theme:root{
+    --back-color: rgb(0, 0, 0);
+    --menu-color1: rgb(252, 223, 94);
+    --font-color: rgb(247, 169, 97);
+    --font-color1: rgb(2, 0, 0);
+}
