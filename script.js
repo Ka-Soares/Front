b216050d548b78dc7ed36e75801d61f8f6a4f833
Ka-Soares <karolinasoares276@gmail.com>
2023-10-30 09:26:22 -0300

    [Front] Estilizando o modo black and white

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-:root{
-    --back-color:  rgba(245,136,35,255);
-    --menu-color: rgb(248, 189, 62);
-    --font-color:   rgba(245,136,35,255);;
-    --font-color1: rgba(87,53,18,255);
-    --sun-moon: rgb(255, 255, 255);
+//declarando o id (DOM)
+ 
+const btnMob = document.querySelector('#btnMob');
+ 
+function toggleMenu(){
+    const navbar = document.querySelector(".nav");
+    navbar.classList.toggle("active");
 }
+btnMob.addEventListener('click', toggleMenu);
 
-.light-theme:root{
-    --back-color: rgb(0, 0, 0);
-    --menu-color1: rgb(252, 223, 94);
-    --font-color: rgb(247, 169, 97);
-    --font-color1: rgb(2, 0, 0);
-}
+// Para declarar as variaveis podemos usar o const ou let 
+const $html = document.querySelector('html')
+const $checkBox = document.querySelector('#switch')
+ 
+$checkBox.addEventListener('change', function(){
+    $html.classList.toggle('light-theme')
+})
